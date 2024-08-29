05eba13ffd3a9a9d72c9fa14ff83069752b07066
Antariksha Rakshit <121787002+antariksha2001@users.noreply.github.com>
2024-08-30 00:24:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 import pandas as pd 
 #from matplotlib import pyplot as plt
-from plotly import graph_objs as go
+#from plotly import graph_objs as go
 from sklearn.linear_model import LinearRegression
 import numpy as np 
 
